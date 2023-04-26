01fd26a220f6970a0262c6af815076ab7c4d22d1
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-04-26 09:59:06 +0300

    Update GlobalStyle.jsx

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 import 'modern-normalize';
 
 export const GlobalStyle = createGlobalStyle`
-    body {
+body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
